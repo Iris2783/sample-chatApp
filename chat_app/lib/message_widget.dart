f5d2568252692cb572101c5b875fbf4ae07e85bd
Iris2783 <iris.corp.2783@gmail.com>
2022-08-09 23:32:47 +0900

    update

---
+++ chat_app/lib/message_widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class MessageWidget extends StatelessWidget {
+  const MessageWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
+          leading: ClipOval(
+            child: Image.asset('assets/img/Original.png'),
+          ),
+          trailing: const Text('3分前'),
+          title: const Text('Mentaくん'),
+          subtitle: const Text('Flutter面白い'),
+        ),
+        ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
+          leading: ClipOval(
+            child: Image.asset('assets/img/Original.png'),
+          ),
+          trailing: const Text('3分前'),
+          title: const Text('Mentaくん'),
+          subtitle: const Text('Flutter面白い'),
+        ),
+        ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
+          leading: ClipOval(
+            child: Image.asset('assets/img/Original.png'),
+          ),
+          trailing: const Text('3分前'),
+          title: const Text('Mentaくん'),
+          subtitle: const Text('Flutter面白い'),
+        ),
+        ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
+          leading: ClipOval(
+            child: Image.asset('assets/img/Original.png'),
+          ),
+          trailing: const Text('3分前'),
+          title: const Text('Mentaくん'),
+          subtitle: const Text('Flutter面白い'),
+        ),
+        ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
+          leading: ClipOval(
+            child: Image.asset('assets/img/Original.png'),
+          ),
+          trailing: const Text('3分前'),
+          title: const Text('Mentaくん'),
+          subtitle: const Text('Flutter面白い'),
+        ),
+        ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
+          leading: ClipOval(
+            child: Image.asset('assets/img/Original.png'),
+          ),
+          trailing: const Text('3分前'),
+          title: const Text('Mentaくん'),
+          subtitle: const Text('Flutter面白い'),
+        ),
+      ],
+    );
+  }
+}
